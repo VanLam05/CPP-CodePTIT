31137d5d9df50fa3f1bc43bf47ab68dc5d5c4b66
Văn Lâm <155084989+VanLam05@users.noreply.github.com>
2024-12-03 22:11:47 +0700

    code ngu bome

--- CHELLO - Hello World.cpp
+++ CHELLO - Hello World.cpp
@@ -1,3 +1,6 @@
+// VanLam 
+// https://www.facebook.com/van.lam.205/
+// ITPTIT Club
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -8,13 +11,18 @@ using namespace std;
 #define vb vector<bool>
 #define pib pair<int, bool>
 const int INF = 1e9 + 7;
+
 void solve()
 {
     cout << "Hello PTIT.";
 }
+
 signed main()
 {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+    
     int t = 1;
     //cin >> t;
     while(t--)
