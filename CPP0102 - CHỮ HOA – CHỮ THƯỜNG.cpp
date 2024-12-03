a49858527a95f0a261ea3744b25d6ba1d2347a25
Văn Lâm <155084989+VanLam05@users.noreply.github.com>
2024-12-03 22:13:25 +0700

    code ngu bome

--- CPP0102 - CHỮ HOA – CHỮ THƯỜNG.cpp
+++ CPP0102 - CHỮ HOA – CHỮ THƯỜNG.cpp
@@ -1,3 +1,6 @@
+// VanLam 
+// https://www.facebook.com/van.lam.205/
+// ITPTIT Club
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -8,6 +11,7 @@ using namespace std;
 #define vb vector<bool>
 #define pib pair<int, bool>
 const int INF = 1e9 + 7;
+
 void solve()
 {
     char ch;
@@ -15,6 +19,7 @@ void solve()
     if (islower(ch)) cout << (char) toupper(ch) << '\n';
     else if (isupper(ch)) cout << (char) tolower(ch) << '\n';
 }
+
 signed main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
